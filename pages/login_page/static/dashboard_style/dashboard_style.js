b083925e55035eda70d26b367c42946c45ec80a7
Makdum cinta C++ <makdumibrohim3@gmail.com>
2024-11-12 16:28:57 +0700

    Menambah pop-up message untuk pemberitahuan kepada pengguna

---
+++ pages/login_page/static/dashboard_style/dashboard_style.js
@@ -0,0 +1,10 @@
+
+// POP UP MESSAGE
+window.onload = function() {
+    const popupMessage = document.getElementById("popupMessage");
+
+    popupMessage.classList.add("show");
+    setTimeout(() => {
+        popupMessage.classList.remove("show");
+    }, 2000);
+};
